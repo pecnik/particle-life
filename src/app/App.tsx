389ba7c786fd6a9421cceae2214661692dbc6161
Blaž Pečnik <blaz@easistent.com>
2023-02-09 17:19:25 +0100

    feat: render particles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { createRenderer } from "./renderer";
+import { createSimulator } from "./simulator";
+
+const renderer = createRenderer();
+const simulator = createSimulator({
+    particleColors: renderer.colors.length,
+    particleCount: 32,
+    worldWidth: 256,
+    worldHeight: 256,
+});
 
 export function App() {
+    const [container, setContainer] = useState<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        let mounted = true;
+        container?.appendChild(renderer.canvas);
+
+        requestAnimationFrame(function frame() {
+            if (mounted) {
+                simulator.update();
+                renderer.render(simulator.particles);
+                requestAnimationFrame(frame);
+            }
+        });
+
+        return () => {
+            mounted = false;
+            container?.removeChild(renderer.canvas);
+        };
+    }, [container]);
     return (
         <div className="w-screen h-screen flex text-gray-100 bg-gray-800">
-            <div className="w-96">Options</div>
-            <div className="w-full h-full bg-black">Viewport</div>
+            <div className="w-64"></div>
+            <div
+                ref={setContainer}
+                className="flex-1 flex flex-wrap justify-center content-center bg-black"
+            />
         </div>
     );
 }
