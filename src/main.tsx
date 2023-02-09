cb2d275db7d3fca51dea200b4a353296c640f1f4
Blaž Pečnik <blaz@easistent.com>
2023-02-09 16:31:59 +0100

    First commit: setup

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root")!);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+);
+
+function App() {
+    return <div>App</div>;
+}
